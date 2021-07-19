41d348dbd62ae901a22e08e627bcb996f63dc6ce
faykris <faykris28@gmail.com>
2021-07-19 15:09:02 -0500

    add 0-binary_tree_node.c and modify header - add mandatory prototypes

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer of tree node
+ * @value: integer number to be added
+ *
+ * Return: pointer new node created
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	if (parent != NULL)
+	{
+		new_node->parent = parent;
+	}
+	return (new_node);
+}
