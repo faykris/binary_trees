8aa8d758dd8109f44587c3d8c327b738528b28dc
faykris <faykris28@gmail.com>
2021-07-19 16:50:01 -0500

    add 1-binary_tree_insert_left.c - incomplete solution, 12 is not here

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: pointer of tree node
+ * @value: integer number to be added
+ *
+ * Return: pointer new node created
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node, *temp;
+    int swap = 0;
+
+    if (parent == NULL)
+    {
+        return (NULL);
+    }
+    temp = parent;
+    new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+    if (temp->left != NULL)
+    {
+        temp = temp->left;
+        swap = temp->n;
+        temp->n = new_node->n;
+        new_node->n = swap;
+
+    }
+    else
+    {
+        temp->left = new_node;
+    }
+    new_node->parent = temp;
+
+	return (new_node);
+}
