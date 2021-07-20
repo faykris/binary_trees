8ab8ddab0c79f85d01f3f69e1cf07f52d0cb32b3
faykris <faykris28@gmail.com>
2021-07-20 12:53:00 -0500

    add 9-binary_tree_height.c - some unsolved edge cases, maybe you can review the left an right insert

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree pointer to be traversed
+ *
+ * Return: height of the binary tree node
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t heightl = 0, heightr = 0, max = 0;
+
+	if (tree)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			return (0);
+		}
+
+		heightl += binary_tree_height(tree->left);
+		heightr += binary_tree_height(tree->right);
+
+		if (heightl >= heightr)
+		{
+			max = heightl;
+		}
+		else
+		{
+			max = heightr;
+		}
+		return (max + 1);
+	}
+
+	return (0);
+}
