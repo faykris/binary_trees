883620de780f9771420209c1fa2940e939233347
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-07-22 14:19:16 -0500

    task 14: change algorithm to get height with recursion

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,41 +1,49 @@
 #include "binary_trees.h"
 
 /**
- * balance - helper to calculate the balance factor
- * @tree: tree pointer to be traversed
+ * height - measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the height.
  *
- * Return: balance factor
+ * Return: height of the tree, 0 otherwise.
  */
-int balance(const binary_tree_t *tree)
+size_t height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
+	size_t height_left, height_rigth;
 
-	if (tree)
-	{
-		return (balance(tree->left) - balance(tree->right) + 1);
-	}
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		height_left = 1 + height(tree->left);
+	else
+		height_left = 1;
+
+	if (tree->right)
+		height_rigth = 1 + height(tree->right);
+	else
+		height_rigth = 1;
+
+	if (height_left > height_rigth)
+		return (height_left);
+	else
+		return (height_rigth);
+
 	return (0);
 }
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: tree pointer to be traversed
+ * binary_tree_balance - measures the balance factor of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the balance factor.
  *
- * Return: balance factor minus 1 when tree node exist
+ * Return: 0 if tree is NULL, otherwise the balance factor.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
+	size_t balance_factor;
 
-	if (tree)
-	{
-		return (balance(tree) - 1);
-	}
-	return (0);
+	if (tree == NULL)
+		return (0);
+
+	balance_factor = height(tree->left) - height(tree->right);
+	return (balance_factor);
 }
